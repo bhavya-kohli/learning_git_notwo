a87291c60c5e3bb9154d578d791398195cab7291
Tanansh-Ahuja <68063129+Tanansh-Ahuja@users.noreply.github.com>
2021-09-06 10:49:28 +0530

    college lab work

---
+++ basiccode.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v(100,0);
+    for(int i=0;i<100;i++)
+    {
+        v[i] = rand()%1000;
+    }
+    int x=v[0];
+    for(int i=0;i<100;i++)
+    {
+        if(v[i]>x)
+        {
+            x=v[i];
+        }
+    }
+    cout<<"Largest element is: "<<x;
+}
